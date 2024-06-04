954c905c1d1ef7f7a041333ce5407078687d2d08
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-04 16:52:03 +0200

    chore: Update dependencies and remove unused code

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics, permissions
 from .models import PortfolioItem
 from .serializers import PortfolioItemSerializer
 
-
 # Create your views here.
 
 class PortfolioItemListCreateView(generics.ListCreateAPIView):
