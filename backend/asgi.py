d310330b76b7944a4cf0d598c1bf600d352032b5
Moonchichiii <matgus1983@gmail.com>
2024-06-17 09:19:41 +0200

    routing updates

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,19 +1,16 @@
 import os
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 import livechat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                livechat.routing.websocket_urlpatterns
-            )
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            livechat.routing.websocket_urlpatterns
         )
     ),
 })
